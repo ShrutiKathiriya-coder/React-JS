c2945c3d0760f0faa259fe29b58eda5a627cbe55
Shruti Kathiriya <kathiriyashruti3@gmail.com>
2025-07-23 19:51:45 +0530

    My First React App

---
+++ app-vite project/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <div>
+          <h1>Name:shruti kathiriya</h1>
+          <h2>gr-id : 7164</h2>
+          <h2>course-name:full-stack</h2>
+          <img src="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8=" alt="" />
+        </div>
+    </>
+  )
+}
+
+export default App
