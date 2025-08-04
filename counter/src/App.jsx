7a9ad98e04b46fe6791509a4c71794d78b95e282
ShrutiKathiriya-coder <12345678+ShrutiKathiriya-coder@users.noreply.github.com>
2025-08-04 18:48:13 +0530

    react-js

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -2,12 +2,8 @@ import { useState } from "react";
 import "../public/css/App.css";
 
 function App() {
-
-  //syntax =let[status,function]=usestate([defaultvalue])
+  // Counter state
   let [count, setCount] = useState(0);
-
-   console.log("Type : ", typeof (count));
-  console.log("Type : ", typeof (setCount));
 
   // Increment 
   const increment = () => {
@@ -19,11 +15,11 @@ function App() {
   };
 
   // Decrement 
-   const decrement = () => {
+  const decrement = () => {
     if (count > 0) {
       setCount(count - 1);
-    }else{
-      alert("minimum limit reached!🙄")
+    } else {
+      alert("Minimum limit reached!🙄");
     }
   };
 
@@ -34,20 +30,43 @@ function App() {
 
   return (
     <div className="container">
-      <h1 className="title">Counter App</h1>
-      <div className="box">
-        <h3 className="value">{count}</h3>
-        <div className="button">
-          <button className="btn increment" onClick={increment}>
-            Increment +
-          </button>
-          <button className="btn decrement" onClick={decrement}>
-            Decrement -
-          </button>
-          <button className="btn reset" onClick={reset}>
-            Reset
-          </button>
+      <h1 className="title">Product Card</h1>
+
+      <div className="card">
+        
+        <img
+          src="https://lolaartdesign.com/cdn/shop/files/1_cd58bb70-88bd-4338-9021-8bffc61182fd.png?v=1687429965&width=1946"
+          alt="Product"
+          className="product-image"
+        />
+
+        
+        <div className="card-content">
+          <h2 className="product-title">Stylish T-Shirt</h2>
+          <p className="product-desc">
+            High-quality cotton T-shirt perfect for casual wear.
+          </p>
+          <p className="product-price">$25</p>
         </div>
+
+       
+        <div className="box">
+          <h3 className="value">{count}</h3>
+          <div className="button">
+            <button className="btn increment" onClick={increment}>
+              Increment +
+            </button>
+            <button className="btn decrement" onClick={decrement}>
+              Decrement -
+            </button>
+            <button className="btn reset" onClick={reset}>
+              Reset
+            </button>
+          </div>
+        </div>
+
+       
+        <button className="btn add-to-cart">Add to Cart 🛒</button>
       </div>
     </div>
   );
